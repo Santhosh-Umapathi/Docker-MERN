beab2b679de940944c166da3da3b950cf1b822a0
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2022-05-13 07:43:23 +0200

    FEAT: All container configs added

--- server/index.js
+++ server/index.js
@@ -41,9 +41,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/values/all", async (req, res) => {
-  const values = await pgClient.query("SELECT * from values");
+  try {
+    const values = await pgClient.query("SELECT * from values");
 
-  res.send(values.rows);
+    res.send(values.rows);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.get("/values/current", async (req, res) => {
